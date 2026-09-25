eaa5b3d276c8553d726dbfc4d94da1d1f3c382ff
amozer1 <amozer2003@yahoo.co.uk>
2026-09-25 11:22:00 +0100

    Updated

--- kpis/ferry/deliverables_utils.py
+++ kpis/ferry/deliverables_utils.py
@@ -9,6 +9,57 @@ def get_metrics(cl32):
         "BIM Set Up",
         "Civil Modelling - Tank",
         "Civil Modelling - Other Assets",
+        "Mechanical Modelling",
+        "Network modelling",
+        "BIM Execution Plan",
+        "Determine entry/exit levels",
+        "Review GI",
+        "Estimate conservative thickness",
+        "Assessment to determine",
+        "Check on shaft sizing",
+        "Produce 2D conept drawing",
+        "MGE Detailed Pile Design",
+        "CAT2 check on MGE pile design",
+        "Manhole Schedule",
+        "Outline drawing",
+        "GA & Long sections",
+        "Valve chamber standard detail",
+        "Kiosk slab proposal",
+        "Shaft penetrations",
+        "Line sizing",
+        "Mechanical calculations",
+        "Pump system curve",
+        "Basis of Design",
+        "Outline P&IDs",
+        "Control Philosophy",
+        "Submission of Outline Design Pack",
+        "Client Review of Design Pack",
+        "Review available GI",
+        "GDR",
+        "Client Review of GDR",
+        "Benching design",
+        "Cover slab design",
+        "Pipe entry/exit",
+        "Pipe exit details",
+        "MH01 & MH02 Design",
+        "Pipework from MHs to Shaft",
+        "Valve Chamber",
+        "Flowmeter Chamber",
+        "Civils Pipe Design",
+        "Tank kiosk enclosure foundation",
+        "Assessment of exit details",
+        "Routing & Design",
+        "HAZOP",
+        "DSEAR Assessment",
+        "Material Take Off",
+        "User Requirement Specification",
+        "Load Schedule",
+        "Power Supply Assessment",
+        "Network Architecture Drawing",
+        "Telemetry Schedules",
+        "Single Line Diagrams",
+        "Block Cable Diagrams",
+        "Final Submission",
     ]
 
     names = (
@@ -18,21 +69,15 @@ def get_metrics(cl32):
         .str.strip()
     )
 
-    deliverables = names.apply(
-        lambda x: any(
-            d.lower() in x.lower()
-            for d in DELIVERABLE_NAMES
-        )
-    )
+    matched = set()
 
-    matched = cl32[deliverables]
+    for name in names:
 
-    st.write("Matched Deliverables")
-    st.dataframe(
-        matched[
-            ["Activity ID", "Activity Name"]
-        ]
-    )
+        for deliverable in DELIVERABLE_NAMES:
+
+            if deliverable.lower() in name.lower():
+                matched.add(deliverable)
+                break
 
     return {
         "total_deliverables": len(matched)
