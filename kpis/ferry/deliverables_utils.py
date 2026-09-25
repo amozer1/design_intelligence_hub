20faca7c130e47c018c8b297c383fb1f271b88d5
amozer1 <amozer2003@yahoo.co.uk>
2026-09-25 11:11:47 +0100

    Updated

--- kpis/ferry/deliverables_utils.py
+++ kpis/ferry/deliverables_utils.py
@@ -1,33 +1,21 @@
+import streamlit as st
+
 def get_metrics(cl32):
 
     ids = (
         cl32["Activity ID"]
         .fillna("")
         .astype(str)
-        .str.strip()
     )
 
-    names = (
-        cl32["Activity Name"]
-        .fillna("")
-        .astype(str)
-        .str.strip()
+    st.write(
+        ids[
+            ~ids.str.startswith("FER-", na=False)
+            & (ids != "")
+        ].tolist()
     )
-
-    deliverables = cl32[
-        ids.str.startswith(
-            "FER-",
-            na=False
-        )
-        &
-        ~ids.str.startswith(
-            "FER-DEP-",
-            na=False
-        )
-        &
-        (names != "")
-    ]
 
     return {
-        "total_deliverables": len(deliverables)
+        "total_deliverables": 0
     }
+``
