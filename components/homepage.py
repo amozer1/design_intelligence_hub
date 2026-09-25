5ac6c079de3b52842fafd015068f19dd03b1aa5f
amozer1 <amozer2003@yahoo.co.uk>
2026-09-25 13:28:26 +0100

    Updated

--- components/homepage.py
+++ components/homepage.py
@@ -14,7 +14,7 @@ from kpis.ferry.deliverables import (
     render as render_deliverables
 )
 
-PAGE_BG = "#D4E4FA"  # Change this colour as required
+PAGE_BG = "#C9D8F0"  # Change this colour as required
 
 
 def render_homepage(
